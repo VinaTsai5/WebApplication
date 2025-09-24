30947ee74f2605aec59b2b6bd9f00b6c9edc3607
Vina Tsai 蔡惠如 <vina.tsai@unitedorthopedic.com>
2025-09-24 21:47:58 +0800

    修改hero-bg

---
+++ api.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+using System.Web;
+
+namespace WebApplication2
+{
+    public partial class api : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //show article.json 
+            // 取得 App_Data\article.json 的絕對路徑
+            string jsonPath = Server.MapPath("~/App_Data/article.json");
+            // 讀取檔案內容
+            string jsonContent = File.ReadAllText(jsonPath);
+            // 輸出到網頁（Content-Type 設為 application/json）
+            Response.ContentType = "application/json";
+            Response.Write(jsonContent);
+            Response.End();
+        }
+    }
+}
